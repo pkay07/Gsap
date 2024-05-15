d132712d0f6ba7d86013e837cb5a7d3545e114d5
pkay <thebeastpkay@gmail.com>
2024-05-16 01:45:42 +0530

    first Project Gsap

---
+++ GSAP_Pro_1/app.js
@@ -0,0 +1,16 @@
+gsap. to ("#box_1", {
+    x: 1000,
+    duration: 2,
+    delay: 1,
+    rotate: 360,
+    backgroundColor: "blue",  // note _1
+    borderRadius: "50%",
+    scale: 0.5
+}) 
+
+
+gsap.from("#box_2", {
+    x:1000,
+    duration:2,
+    delay:1
+})
